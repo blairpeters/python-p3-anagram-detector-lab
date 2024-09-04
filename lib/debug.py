29e87416ec25ac775eb48152c70e640d61fea88d
Tony Blair Maina <blairpeters13@gmail.com>
2024-09-04 12:39:25 +0300

    completes lab

--- lib/debug.py
+++ lib/debug.py
@@ -4,3 +4,5 @@ from anagram import Anagram
 
 if __name__ == '__main__':
     import ipdb; ipdb.set_trace()
+    anagram = Anagram("listen")
+    print(anagram.match(["enlists", "google", "inlets", "banana"]))
